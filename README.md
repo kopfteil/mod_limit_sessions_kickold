b8908f73e1e5c483e345545a1feabaaf681abe6f
bret <bret@milp.dyndns.org>
2017-01-19 19:17:28 +0100

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mod_limit_sessions_kickold
 Prosody XMPP Server plugin in lua to limit concurrent sessions a client can have and kick the oldest on exceed
+
+I did not make this plugin, i only adapted it to kick the oldest,
+instead of newest session.
+
+This is the original:
+
+https://modules.prosody.im/mod_c2s_limit_sessions.html
---
+++ mod_limit_sessions_kickold.lua
@@ -0,0 +1,23 @@
+-- mod_c2s_limit_sessions
+
+local next, count = next, require "util.iterators".count;
+
+local max_resources = module:get_option_number("max_resources", 1);
+
+local sessions = hosts[module.host].sessions;
+module:hook("resource-bind", function(event)
+	local session = event.session;
+	if count(next, sessions[session.username].sessions) > max_resources then
+
+		for resource, session in pairs(sessions[session.username].sessions) 
+		do if session ~= event.session then 
+		session:close{ condition = "policy-violation", text = "Too many resources" };
+		end end
+		return false
+	end
+end, -1);
+
+for resource, session in pairs(sessions[session.username].sessions) 
+do if session ~= event.session then 
+session:close() end end
+
